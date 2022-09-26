9324b6821a10d587d82507e32ed74d3b28b9a806
CocoLee <2313988763@qq.com>
2022-09-26 17:39:09 +0800

    init English-website

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Wellcome to cocoball.vip.',
+  description: '记录学习英语笔记',
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Word', link: '/guide/word/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Phonetic', link: 'https://cocoball.vip/#/yinbiao/yinbiao' },
+    ]
+  }
+}
